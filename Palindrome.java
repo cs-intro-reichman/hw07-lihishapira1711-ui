ad0b04852dbb8bafe4cbec5bec3f909e344f5869
lihishapi <lihi.shapira@post.runi.ac.il>
2026-01-01 14:54:51 +0200

    Completed Assignment

--- Palindrome.java
+++ Palindrome.java
@@ -3,11 +3,25 @@ public class Palindrome {
 
 	public static void main(String[]args) {
     	System.out.println(isPalindrome(args[0]));
+
     }
 	
 	/** Checks if the given string is a palindrome. */
 	public static boolean isPalindrome(String s) {
-		//// Replace the followaing statement with your code
-		return false;
+		int start=0;
+		int end= s.length()-1;
+		return isPalindrome (s,start,end);
+    }
+	public static boolean isPalindrome(String s, int start, int end) {
+		if (start==end || end == -1)
+		{
+			return true;
+		}
+		if (s.charAt(start) != s.charAt(end))
+		{
+			return false;
+		}
+		return isPalindrome (s, start+1, end-1);
+		
     }
 }
