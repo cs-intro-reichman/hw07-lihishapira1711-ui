152331cd3af94e478854bd6d5ba0765d33958f9e
lihishapi <lihi.shapira@post.runi.ac.il>
2026-01-01 16:54:54 +0200

    Completed Assignment

--- IsSorted.java
+++ IsSorted.java
@@ -2,13 +2,20 @@ public class IsSorted {
 
     // Public function: called by the user
     public static boolean isSorted(int[] a) {
-        return isSorted(a, 0);
+        return isSorted(a, a.length-2);
     }
 
     // Helper recursive function
     private static boolean isSorted(int[] a, int i) {
-        //// Replace the following statement with your code
-        return false; 
+        if (i < 0)
+        {
+            return true;
+        }
+        if (a[i]>a[i+1])
+        {
+         return false;    
+        }
+        return isSorted (a,i-1); 
     }
 
     public static void main(String[] args) {
--- Koch.java
+++ Koch.java
@@ -8,35 +8,62 @@ public class Koch {
 		//// Uncomment only one block in each test, and remember to compile
 		//// the class whenever you change the test.
 
-        /*
+        
 		// Tests the curve function:
 		// Gets n, x1, y1, x2, y2,
-		// and draws a Koch curve of depth n from (x1,y1) to (x2,y2).
+		//and draws a Koch curve of depth n from (x1,y1) to (x2,y2).
 		curve(Integer.parseInt(args[0]),
 			  Double.parseDouble(args[1]), Double.parseDouble(args[2]), 
 		      Double.parseDouble(args[3]), Double.parseDouble(args[4]));
-		*/
+		
 
-		/*
+		
 		// Tests the snowflake function:
 		// Gets n, and draws a Koch snowflake of n edges in the standard canvass.
 		snowFlake(Integer.parseInt(args[0]));
-		*/
+		
 	}
 
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
 		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
+		  if (n == 0) {
+        StdDraw.line(x1, y1, x2, y2);
+        return;
+		  }
+
+    double dx = (x2 - x1) / 3.0;
+    double dy = (y2 - y1) / 3.0;
+
+    double xA = x1 + dx;
+    double yA = y1 + dy;
+
+    double xB = x1 + 2 * dx;
+    double yB = y1 + 2 * dy;
+
+    double xPeak = xA + (dx - Math.sqrt(3) * dy) / 2;
+    double yPeak = yA + (dy + Math.sqrt(3) * dx) / 2;
+
+    curve(n - 1, x1, y1, xA, yA);
+    curve(n - 1, xA, yA, xPeak, yPeak);
+    curve(n - 1, xPeak, yPeak, xB, yB);
+    curve(n - 1, xB, yB, x2, y2);
+
 	}
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
-	public static void snowFlake(int n) {
-		// A little tweak that makes the drawing look better
-		StdDraw.setYscale(0, 1.1);
-		StdDraw.setXscale(0, 1.1);
-		// Draws a Koch snowflake of depth n
-		//// Write the rest of your code below.
-	}
+public static void snowFlake(int n) {
+
+    StdDraw.setXscale(0, 1.1);
+    StdDraw.setYscale(0, 1.1);
+
+    double x1 = 0.1, y1 = 0.2;
+    double x2 = 1.0, y2 = 0.2;
+    double x3 = 0.55, y3 = 0.2 + Math.sqrt(3) / 2 * 0.9;
+
+    curve(n, x1, y1, x2, y2);
+    curve(n, x2, y2, x3, y3);
+    curve(n, x3, y3, x1, y1);
+}
 }
